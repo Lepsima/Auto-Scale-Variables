46ee93a48f1147684de7df386497fd131b9b2ae0
Lepsima <hugo.diaz.morcillo@gmail.com>
2026-01-21 16:48:25 +0100

    Version change

--- AutoVariables/Editor/Value2PropertyDrawer.cs
+++ AutoVariables/Editor/Value2PropertyDrawer.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 
 [CustomPropertyDrawer(typeof(Value2), true)]
 public class Value2PropertyDrawer : PropertyDrawer {
-	
+
 	public override VisualElement CreatePropertyGUI(SerializedProperty property) {
 		VisualElement container = new();
 		string help = property.type switch {
