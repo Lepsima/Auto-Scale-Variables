9d385ddf4ef548765b4390878bc011ccfabe6f8b
Lepsima <hugo.diaz.morcillo@gmail.com>
2026-01-21 17:41:22 +0100

    AngleBetween

--- AutoVariables/Global/VMath.cs
+++ AutoVariables/Global/VMath.cs
@@ -5,6 +5,7 @@ namespace AutoScaleVariables {
 public static class VMath {
 	public static Force Clamp(Force force, float min, float	max) => new(Mathf.Clamp(force.value, min, max));
 	
-	public static Angle AngleBetween(Value3 a, Value3 b) => Degree(Vector3.Angle(a.value, b.value));
+	public static Angle AngleBetween(Direction2 a, Direction2 b) => Degree(Vector2.Angle(a.value, b.value));
+	public static Angle AngleBetween(Direction3 a, Direction3 b) => Degree(Vector3.Angle(a.value, b.value));
 }
 }
